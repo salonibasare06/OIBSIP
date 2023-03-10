17d8e2d2ac7ea48394c16b84e2cecee267c3deb1
salonibasare06 <121510126+salonibasare06@users.noreply.github.com>
2023-03-10 23:17:32 +0530

    Add files via upload

---
+++ LEVEL 2/TASK 1/calculator.js
@@ -0,0 +1,26 @@
+(function() {
+   let display= document.querySelector('.display')
+   let buttons= document.querySelectorAll('.btn') 
+   let clear= document.querySelector('.btn-clear') 
+   let equal= document.querySelector('.btn-equal')  
+
+   buttons.forEach(function(button){
+    button.addEventListener('click',function(e){
+        let value=e.target.dataset.num;
+        display.value+=value;
+    })
+   });
+
+   equal.addEventListener('click',function(e){
+    if(display.value===" "){
+        display.value='please enter';
+    }else{
+        let answer=eval(display.value);
+        display.value=answer;
+    }
+})
+clear.addEventListener("click",function(e){
+    display.value="  ";
+})
+
+})();
